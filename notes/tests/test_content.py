51291ca37dc3e2a98f71aa7e9c901d3b2aa968f3
Алексей Ларионов <Alakey060711@yandex.ru>
2023-06-27 03:36:21 +0500

    Small fix

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
 
 from notes.models import Note
@@ -11,34 +11,33 @@ class TestContent(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.users = User.objects.create(username='Юзер')
-        cls.another_users = User.objects.create(username='ЮзерД')
+        cls.user = User.objects.create(username='Юзер')
+        cls.user_client = Client()
+        cls.user_client.force_login(cls.user)
+        cls.another_user = User.objects.create(username='ЮзерД')
         cls.notes = Note.objects.create(
-            title='Заголовок', text='Текст', author=cls.users
+            title='Заголовок', text='Текст', author=cls.user
         )
 
     def test_note_in_list_for_author(self):
         url = reverse('notes:list')
-        self.client.force_login(self.users)
-        response = self.client.get(url)
+        response = self.user_client.get(url)
         object_list = response.context['object_list']
         self.assertIn(self.notes, object_list)
 
     def test_note_not_in_list_for_another_user(self):
         url = reverse('notes:list')
-        self.client.force_login(self.another_users)
+        self.client.force_login(self.another_user)
         response = self.client.get(url)
         object_list = response.context['object_list']
         self.assertNotIn(self.notes, object_list)
 
     def test_create_note_page_contains_form(self):
         url = reverse('notes:add')
-        self.client.force_login(self.users)
-        response = self.client.get(url)
+        response = self.user_client.get(url)
         self.assertIn('form', response.context)
 
     def test_edit_note_page_contains_form(self):
         url = reverse('notes:edit', args=(self.notes.slug,))
-        self.client.force_login(self.users)
-        response = self.client.get(url)
+        response = self.user_client.get(url)
         self.assertIn('form', response.context)
